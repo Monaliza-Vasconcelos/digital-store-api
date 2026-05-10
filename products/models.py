767a81e671cf1eb002fdd4f6a3d45ad92f289afb
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2026-05-10 12:18:25 -0300

    initial backend setup

---
+++ products/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price_discount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+
+    reference = models.TextField(blank=True, default="")
+    description = models.TextField(blank=True, default="")
+    rating = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="images"
+    )
+
+    image = models.URLField()
+
+    def __str__(self):
+        return f"Imagem de {self.product.name}"
