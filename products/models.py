730b462082d8609b2fe27f0ac7688d1325848346
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2026-05-10 14:56:29 -0300

    Update models.py

--- products/models.py
+++ products/models.py
@@ -3,11 +3,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    price_discount = models.DecimalField(
-        max_digits=10,
-        decimal_places=2,
-        default=0
-    )
+    price_discount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     reference = models.TextField(blank=True, default="")
     description = models.TextField(blank=True, default="")
@@ -18,13 +14,11 @@ class Product(models.Model):
 
 
 class ProductImage(models.Model):
-
     product = models.ForeignKey(
         Product,
         on_delete=models.CASCADE,
         related_name="images"
     )
-
     image = models.URLField()
 
     def __str__(self):
